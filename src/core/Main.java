5c8eb076ce58672b8ad29b84aefe140b6f56f8a5
Tom Apeltauer <apelttom@gmail.com>
2017-11-06 13:06:10 +0100

    NEW: initial Eclipse project commit

---
+++ src/core/Main.java
@@ -0,0 +1,13 @@
+package core;
+
+import graphicalUserInterface.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		TitleWindow titleWindow = new TitleWindow();
+		titleWindow.setSize(300, 200);
+		titleWindow.setVisible(true);
+	}
+
+}
---
+++ src/graphicalUserInterface/titleWindow.java
@@ -0,0 +1,13 @@
+package graphicalUserInterface;
+
+import java.awt.Frame;  // Using Frame class in package java.awt
+
+public class TitleWindow extends Frame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5787900686470589962L;
+	
+
+}
